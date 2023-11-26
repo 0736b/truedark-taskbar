f2e9c87f74ee0e25daf072c4b8311633eb0e5b09
Thanawat <76035626+0736b@users.noreply.github.com>
2023-11-26 16:11:05 +0700

    add: disable shortcut arrow icon

---
+++ disable_shortcut_arrow_icon.bat
@@ -0,0 +1,20 @@
+@echo off
+
+REM Check for admin rights
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo This script requires administrator privileges.
+    echo Please run the script as an administrator.
+    pause
+    exit /b
+)
+
+echo Editing Explorer's shell icon registry value...
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" /v "29" /t REG_SZ /d "" /f
+echo Registry value edited successfully.
+
+echo Restarting Windows Explorer...
+taskkill /f /im explorer.exe
+start explorer.exe
+
+echo Disabled shortcut arrow icon!.
